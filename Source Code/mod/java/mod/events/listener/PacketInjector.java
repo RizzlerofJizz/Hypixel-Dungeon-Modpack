7102f69ff44d1d33748ef186713447c5e9c74cb6
JimmyJamMan <142516840+JimmyJamMan@users.noreply.github.com>
2023-08-18 22:58:41 +0800

    Add files via upload

---
+++ Source Code/mod/java/mod/events/listener/PacketInjector.java
@@ -0,0 +1,112 @@
+/*
+ * Dungeons Guide - The most intelligent Hypixel Skyblock Dungeons Mod
+ * Copyright (C) 2021  cyoung06
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package kr.syeyoung.dungeonsguide.mod.events.listener;
+
+import io.netty.channel.*;
+import kr.syeyoung.dungeonsguide.mod.events.impl.PacketProcessedEvent;
+import kr.syeyoung.dungeonsguide.mod.events.impl.PlayerInteractEntityEvent;
+import kr.syeyoung.dungeonsguide.mod.events.impl.RawPacketReceivedEvent;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.client.C02PacketUseEntity;
+import net.minecraft.network.play.client.C0EPacketClickWindow;
+import net.minecraft.network.play.client.C0FPacketConfirmTransaction;
+import net.minecraft.network.play.server.*;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@ChannelHandler.Sharable
+public class PacketInjector extends ChannelDuplexHandler {
+
+    private static Set<Class> targetedPackets = new HashSet<>();
+    static {
+        targetedPackets.add(S04PacketEntityEquipment.class);
+        targetedPackets.add(S45PacketTitle.class);
+        targetedPackets.add(S38PacketPlayerListItem.class);
+        targetedPackets.add(S30PacketWindowItems.class);
+        targetedPackets.add(S2FPacketSetSlot.class);
+        targetedPackets.add(S23PacketBlockChange.class);
+        targetedPackets.add(S22PacketMultiBlockChange.class);
+        targetedPackets.add(S3BPacketScoreboardObjective.class);
+        targetedPackets.add(S3CPacketUpdateScore.class);
+        targetedPackets.add(S3DPacketDisplayScoreboard.class);
+        targetedPackets.add(S3EPacketTeams.class);
+        targetedPackets.add(S34PacketMaps.class);
+    }
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        Packet packet = (Packet) msg;
+        boolean doStuff = targetedPackets.contains(msg.getClass());
+        try {
+            if (doStuff) {
+                RawPacketReceivedEvent receivedEvent = new RawPacketReceivedEvent(packet);
+                MinecraftForge.EVENT_BUS.post(receivedEvent);
+                packet = receivedEvent.packet;
+            }
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+
+        // Hopefully this works? idk
+        Packet finalPacket = packet;
+        if (doStuff)
+            Minecraft.getMinecraft().addScheduledTask(() -> {
+                MinecraftForge.EVENT_BUS.post(new PacketProcessedEvent.Pre(finalPacket));
+            });
+        super.channelRead(ctx, packet);
+        if (doStuff)
+            Minecraft.getMinecraft().addScheduledTask(() -> {
+                MinecraftForge.EVENT_BUS.post(new PacketProcessedEvent.Post(finalPacket));
+            });
+    }
+
+    @Override
+    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+        Packet packet = (Packet) msg;
+        if (packet instanceof C02PacketUseEntity) {
+            C02PacketUseEntity packet2 = (C02PacketUseEntity) packet;
+            PlayerInteractEntityEvent piee;
+            if (packet2.getAction() == C02PacketUseEntity.Action.ATTACK)
+                piee = new PlayerInteractEntityEvent(true, packet2.getEntityFromWorld(Minecraft.getMinecraft().theWorld));
+            else
+                piee = new PlayerInteractEntityEvent(false, ((C02PacketUseEntity) packet).getEntityFromWorld(Minecraft.getMinecraft().theWorld));
+            if (MinecraftForge.EVENT_BUS.post(piee)) return;
+        }
+        super.write(ctx, msg, promise);
+    }
+
+
+    @SubscribeEvent
+    public void onServerConnect(FMLNetworkEvent.ClientConnectedToServerEvent event) {
+        event.manager.channel().pipeline().addBefore("packet_handler", "dg_packet_handler", this);
+    }
+
+    public void cleanup() {
+        try {
+            if (Minecraft.getMinecraft().getNetHandler() != null)
+                Minecraft.getMinecraft().getNetHandler().getNetworkManager().channel().pipeline().remove("dg_packet_handler");
+        } catch (Exception e) {
+
+        }
+    }
+}
---
+++ Source Code/mod/java/mod/events/listener/PacketListener.java
@@ -0,0 +1,222 @@
+/*
+ * Dungeons Guide - The most intelligent Hypixel Skyblock Dungeons Mod
+ * Copyright (C) 2023  cyoung06 (syeyoung)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package kr.syeyoung.dungeonsguide.mod.events.listener;
+
+import kr.syeyoung.dungeonsguide.mod.DungeonsGuide;
+import kr.syeyoung.dungeonsguide.mod.SkyblockStatus;
+import kr.syeyoung.dungeonsguide.mod.events.impl.*;
+import kr.syeyoung.dungeonsguide.mod.features.FeatureRegistry;
+import kr.syeyoung.dungeonsguide.mod.parallelUniverse.map.MapDataManager;
+import kr.syeyoung.dungeonsguide.mod.parallelUniverse.scoreboard.Objective;
+import kr.syeyoung.dungeonsguide.mod.parallelUniverse.scoreboard.ScoreboardManager;
+import kr.syeyoung.dungeonsguide.mod.parallelUniverse.tab.TabList;
+import kr.syeyoung.dungeonsguide.mod.parallelUniverse.tab.TabListEntry;
+import kr.syeyoung.dungeonsguide.mod.parallelUniverse.teams.NameTagVisibility;
+import kr.syeyoung.dungeonsguide.mod.parallelUniverse.teams.Team;
+import kr.syeyoung.dungeonsguide.mod.parallelUniverse.teams.TeamManager;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.*;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.Tuple;
+import net.minecraft.world.storage.MapData;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class PacketListener {
+    private final SkyblockStatus skyblockStatus = DungeonsGuide.getDungeonsGuide().getSkyblockStatus();
+
+    @SubscribeEvent
+    public void onPacketReceive(RawPacketReceivedEvent event) {
+        try {
+            Packet packet = event.packet;
+            if (skyblockStatus.isOnSkyblock()
+                    && packet instanceof S04PacketEntityEquipment
+                    && FeatureRegistry.FIX_SPIRIT_BOOTS.isEnabled()) { // Inventory packet name
+                S04PacketEntityEquipment packet2 = (S04PacketEntityEquipment) packet;
+                if (packet2.getEntityID() == Minecraft.getMinecraft().thePlayer.getEntityId()) {
+                    packet2 = new S04PacketEntityEquipment(packet2.getEntityID(), packet2.getEquipmentSlot() + 1, packet2.getItemStack());
+                    packet = packet2;
+                }
+            }
+            event.packet = packet;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SubscribeEvent
+    public void packetProcessPost(PacketProcessedEvent.Post post) {
+        Packet packet = post.packet;
+        if (packet instanceof S30PacketWindowItems) {
+            MinecraftForge.EVENT_BUS.post(new WindowUpdateEvent((S30PacketWindowItems) packet, null));
+        } else if (packet instanceof S2FPacketSetSlot) {
+            MinecraftForge.EVENT_BUS.post(new WindowUpdateEvent( null, (S2FPacketSetSlot) packet));
+        } else if (packet instanceof S23PacketBlockChange) {
+            BlockUpdateEvent blockUpdateEvent = new BlockUpdateEvent.Post();
+            blockUpdateEvent.getUpdatedBlocks().add(new Tuple<>(
+                    ((S23PacketBlockChange) packet).getBlockPosition(), ((S23PacketBlockChange) packet).getBlockState()));
+            MinecraftForge.EVENT_BUS.post(blockUpdateEvent);
+        } else if (packet instanceof S22PacketMultiBlockChange) {
+            BlockUpdateEvent blockUpdateEvent = new BlockUpdateEvent.Post();
+            for (S22PacketMultiBlockChange.BlockUpdateData changedBlock : ((S22PacketMultiBlockChange) packet).getChangedBlocks()) {
+                blockUpdateEvent.getUpdatedBlocks().add(new Tuple<>(changedBlock.getPos(), changedBlock.getBlockState()));
+            }
+            MinecraftForge.EVENT_BUS.post(blockUpdateEvent);
+        }else if (packet instanceof S45PacketTitle) {
+            MinecraftForge.EVENT_BUS.post(new TitleEvent((S45PacketTitle) packet));
+        } else if (packet instanceof S38PacketPlayerListItem) {
+            MinecraftForge.EVENT_BUS.post(new PlayerListItemPacketEvent((S38PacketPlayerListItem) packet));
+        }else if (packet instanceof S34PacketMaps) {
+            MapData mapData = MapDataManager.INSTANCE.createMapData(((S34PacketMaps) packet).getMapId());
+            try {
+                ((S34PacketMaps) packet).setMapdataTo(mapData);
+            } catch (Exception ignored) {} // hypixel seem to be sending bad map datas.
+            MinecraftForge.EVENT_BUS.post(new MapUpdateEvent(((S34PacketMaps) packet).getMapId(), mapData));
+        }
+    }
+
+    @SubscribeEvent
+    public void onPrePacketProcess(PacketProcessedEvent.Pre event) {
+        Packet packet =event.packet;
+        if (event.packet instanceof S23PacketBlockChange) {
+            BlockUpdateEvent blockUpdateEvent = new BlockUpdateEvent.Pre();
+            blockUpdateEvent.getUpdatedBlocks().add(new Tuple<>(
+                    ((S23PacketBlockChange) event.packet).getBlockPosition(),
+                    ((S23PacketBlockChange) event.packet).getBlockState()));
+            MinecraftForge.EVENT_BUS.post(blockUpdateEvent);
+        } else if (event.packet instanceof S22PacketMultiBlockChange) {
+            BlockUpdateEvent blockUpdateEvent = new BlockUpdateEvent.Pre();
+            for (S22PacketMultiBlockChange.BlockUpdateData changedBlock : ((S22PacketMultiBlockChange) event.packet).getChangedBlocks()) {
+                blockUpdateEvent.getUpdatedBlocks().add(new Tuple<>(changedBlock.getPos(), changedBlock.getBlockState()));
+            }
+            MinecraftForge.EVENT_BUS.post(blockUpdateEvent);
+        } else if (packet instanceof S3BPacketScoreboardObjective) {
+            S3BPacketScoreboardObjective objectivePkt = (S3BPacketScoreboardObjective) packet;
+            if (objectivePkt.func_149338_e() == 2) {
+                Objective objective = ScoreboardManager.INSTANCE.getObjective(objectivePkt.func_149339_c());
+                if (objective != null) {
+                    objective.setDisplayName(objectivePkt.func_149337_d());
+                    objective.setDisplayType(objectivePkt.func_179817_d());
+                }
+            } else if (objectivePkt.func_149338_e() == 1) {
+                ScoreboardManager.INSTANCE.removeObjective(objectivePkt.func_149339_c());
+            } else if (objectivePkt.func_149338_e() == 0) {
+                Objective objective = new Objective(objectivePkt.func_149339_c());
+                objective.setDisplayName(objectivePkt.func_149337_d());
+                objective.setDisplayType(objectivePkt.func_179817_d());
+                ScoreboardManager.INSTANCE.addObjective(objective);
+            }
+        } else if (packet instanceof S3CPacketUpdateScore) {
+            S3CPacketUpdateScore score = (S3CPacketUpdateScore) packet;
+            Objective objective = ScoreboardManager.INSTANCE.getObjective(score.getObjectiveName());
+            if (objective != null) {
+                if (score.getScoreAction() == S3CPacketUpdateScore.Action.CHANGE) {
+                    objective.updateScore(score.getPlayerName(), score.getScoreValue());
+                } else if (score.getScoreAction() == S3CPacketUpdateScore.Action.REMOVE) {
+                    objective.removeScore(score.getPlayerName());
+                }
+            }
+        } else if (packet instanceof S3DPacketDisplayScoreboard) {
+            S3DPacketDisplayScoreboard board = (S3DPacketDisplayScoreboard) packet;
+            ScoreboardManager.INSTANCE.displayScoreboard(board.func_149371_c(), board.func_149370_d());
+        } else if (packet instanceof S3EPacketTeams) {
+            S3EPacketTeams pkt = (S3EPacketTeams) packet;
+            if (pkt.getAction() == 0) {
+                // CREATE
+                Team team = new Team(pkt.getName());
+                team.setDisplayName(pkt.getDisplayName());
+                team.setPrefix(pkt.getPrefix());
+                team.setSuffix(pkt.getSuffix());
+                team.setNameTagVisibility(NameTagVisibility.of(pkt.getNameTagVisibility()));
+                team.setColor(EnumChatFormatting.func_175744_a(pkt.getColor()));
+
+                for (String player : pkt.getPlayers()) {
+                    team.addTeamMember(player);
+                }
+
+                TeamManager.INSTANCE.createTeam(team);
+            } else if (pkt.getAction() == 1) {
+                // REMOVE
+                TeamManager.INSTANCE.removeTeam(pkt.getName());
+            } else if (pkt.getAction() == 2) {
+                // UPDATE
+                Team team = TeamManager.INSTANCE.getTeamByName(pkt.getName());
+                if (team != null) {
+                    team.setDisplayName(pkt.getDisplayName());
+                    team.setPrefix(pkt.getPrefix());
+                    team.setSuffix(pkt.getSuffix());
+                    team.setNameTagVisibility(NameTagVisibility.of(pkt.getNameTagVisibility()));
+                    team.setColor(EnumChatFormatting.func_175744_a(pkt.getColor()));
+                }
+            } else if (pkt.getAction() == 3) {
+                // PLAYER UPDATE
+                Team team = TeamManager.INSTANCE.getTeamByName(pkt.getName());
+                if (team != null) {
+                    for (String player : pkt.getPlayers()) {
+                        team.addTeamMember(player);
+                    }
+                }
+            } else if (pkt.getAction() == 4) {
+                // PLAYER REMOVE
+                Team team = TeamManager.INSTANCE.getTeamByName(pkt.getName());
+                if (team != null) {
+                    for (String player : pkt.getPlayers()) {
+                        team.removeTeamMember(player);
+                    }
+                }
+            }
+        }  else if (packet instanceof S38PacketPlayerListItem) {
+            S38PacketPlayerListItem pkt = (S38PacketPlayerListItem) packet;
+            S38PacketPlayerListItem.Action action = pkt.getAction();
+            if (action == S38PacketPlayerListItem.Action.ADD_PLAYER) {
+                for (S38PacketPlayerListItem.AddPlayerData entry : pkt.getEntries()) {
+                    TabListEntry tabListEntry = new TabListEntry(entry.getProfile(), entry.getGameMode());
+                    tabListEntry.setPing(entry.getPing());
+                    tabListEntry.setDisplayName(entry.getDisplayName());
+                    TabList.INSTANCE.updateEntry(tabListEntry);
+                }
+            } else if (action == S38PacketPlayerListItem.Action.REMOVE_PLAYER) {
+                for (S38PacketPlayerListItem.AddPlayerData entry : pkt.getEntries()) {
+                    TabList.INSTANCE.removeEntry(entry.getProfile().getId());
+                }
+            } else if (action == S38PacketPlayerListItem.Action.UPDATE_LATENCY) {
+                for (S38PacketPlayerListItem.AddPlayerData entry : pkt.getEntries()) {
+                    TabListEntry entry1 = TabList.INSTANCE.getEntry(entry.getProfile().getId());
+                    if (entry1 != null) entry1.setPing(entry.getPing());
+                }
+            } else if (action == S38PacketPlayerListItem.Action.UPDATE_DISPLAY_NAME) {
+                for (S38PacketPlayerListItem.AddPlayerData entry : pkt.getEntries()) {
+                    TabListEntry entry1 = TabList.INSTANCE.getEntry(entry.getProfile().getId());
+                    if (entry1 != null) entry1.setDisplayName(entry.getDisplayName());
+                }
+            } else if (action == S38PacketPlayerListItem.Action.UPDATE_GAME_MODE) {
+                for (S38PacketPlayerListItem.AddPlayerData entry : pkt.getEntries()) {
+                    TabListEntry entry1 = TabList.INSTANCE.getEntry(entry.getProfile().getId());
+                    TabListEntry neu = new TabListEntry(entry1.getGameProfile(), entry.getGameMode());
+                    neu.setPing(entry1.getPing());
+                    neu.setDisplayName(entry1.getDisplayName());
+
+                    TabList.INSTANCE.updateEntry(neu);
+                }
+            }
+        }
+    }
+
+}
